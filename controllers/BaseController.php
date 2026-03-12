3cfb2b59a796c1908e2a0ff444e8d12acd81c63a
ryuu <jmonvil12>
2026-03-12 10:03:44 +0100

    feat: add BaseController with JSON response and input handling methods

---
+++ controllers/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+class BaseController {
+    protected function json($data, $status = 200): void {
+        http_response_code($status);
+        header('Content-Type: application/json; charset=UTF-8');
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    }
+
+    protected function getJsonInput(): array {
+        $body = json_decode(file_get_contents('php://input'), true);
+        return is_array($body) ? $body : [];
+    }
+}
